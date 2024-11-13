57dfb6d6b188efdd924343a1ecbef01615eb348a
Noah Taiga Endo <noah.eyeing@gmail.com>
2024-11-13 19:39:33 +0900

    install react hook form

---
+++ src/modules/index/components/form/page.tsx
@@ -0,0 +1,9 @@
+import { Box } from "@mui/material";
+
+export const Form = () => {
+  return (
+    <Box>
+      <h1>Form</h1>
+    </Box>
+  )
+}
---
+++ src/modules/index/page.tsx
@@ -0,0 +1,9 @@
+import { Box } from "@mui/material";
+
+export const Index = () => {
+  return (
+    <Box>
+      <h1>On Time</h1>
+    </Box>
+  )
+}
